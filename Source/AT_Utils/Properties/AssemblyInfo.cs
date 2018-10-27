8a83ce4b3c923ee46932c060d83f38d41e16708d
Lisias T <me@lisias.net>
2018-10-27 10:05:50 -0200

    VersioVersioning Normalizing

--- Source/AT_Utils/Properties/AssemblyInfo.cs
+++ Source/AT_Utils/Properties/AssemblyInfo.cs
@@ -24,12 +24,11 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-#if NIGHTBUILD
-[assembly: AssemblyVersion("1.5.*")]
-#else
-[assembly: AssemblyVersion("1.6.2.0")]
-#endif
-[assembly: KSPAssembly("AT_Utils", 1, 6)]
+[assembly: AssemblyVersion(AT_Utils.Version.Number)]
+[assembly: AssemblyFileVersion(AT_Utils.Version.Number)]
+
+[assembly: KSPAssembly("AT_Utils", AT_Utils.Version.major, AT_Utils.Version.minor)]
+//[assembly: KSPAssemblyDependency("KSPe", 2, 0)]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -37,17 +36,17 @@ using System.Reflection;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-namespace AT_Utils
-{
-    public class ModInfo : KSP_AVC_Info
-    {
-        public ModInfo()
-        {
-            MinKSPVersion = new Version(1,4,5);
-            MaxKSPVersion = new Version(1,4,5);
+//namespace AT_Utils
+//{
+//    public class ModInfo : KSP_AVC_Info
+//    {
+//        public ModInfo()
+//        {
+//            MinKSPVersion = new Version(1,4,5);
+//            MaxKSPVersion = new Version(1,4,5);
 
-            VersionURL   = "https://raw.githubusercontent.com/allista/AT_Utils/master/GameData/000_AT_Utils/000_AT_Utils.version";
-            UpgradeURL   = "https://github.com/allista/AT_Utils/releases";
-        }
-    }
-}
+//            VersionURL   = "https://raw.githubusercontent.com/allista/AT_Utils/master/GameData/000_AT_Utils/000_AT_Utils.version";
+//            UpgradeURL   = "https://github.com/allista/AT_Utils/releases";
+//        }
+//    }
+//}
